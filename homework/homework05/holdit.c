cdc4bd52ccec535881952ef7fa3e4b6f813f0868
ThomasOsayi <thomasosayi@gmail.com>
2025-11-05 11:13:22 -0800

    finalized changes/check

--- homework/homework05/holdit.c
+++ homework/homework05/holdit.c
@@ -6,7 +6,7 @@ int main(void) {
     double duration;
     int minutes, seconds;
     
-    /* Display instructions */
+    
     printf("This program will time how long you can hold your breath.\n");
     printf("Take a deep breath, then press the 'Enter' key.\n");
     printf("When you absolutely have to exhale, press the enter key again.\n");
@@ -14,24 +14,22 @@ int main(void) {
     
     printf("Press Enter to start timing...");
     
-    /* Wait for first Enter press and record start time */
+    
     getchar();
     start_time = time(NULL);
     
     printf("Holding... Press Enter when you need to breathe!\n");
     
-    /* Wait for second Enter press and record end time */
+    
     getchar();
     end_time = time(NULL);
     
-    /* Calculate duration in seconds */
+
     duration = difftime(end_time, start_time);
-    
-    /* Convert to minutes and seconds */
     minutes = (int)duration / 60;
     seconds = (int)duration % 60;
     
-    /* Display result */
+   
     printf("\nYou held your breath for: %d minute(s) and %d second(s)\n", minutes, seconds);
     
     return 0;
